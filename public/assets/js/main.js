ee677fa065bd67979aad34e55070506fc678de43
J.Dareenat <106639881+Dareenatj@users.noreply.github.com>
2025-07-02 16:43:24 -0400

    delete old files

--- public/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// Example: Alert on form submission
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  if (form) {
-    form.addEventListener("submit", () => alert("Form submitted!"));
-  }
-});
